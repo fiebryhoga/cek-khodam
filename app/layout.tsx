55afdd370e286ecdf8df0446a12b9cc76a7d0fec
fiebryhoga <dimasfiebry@gmail.com.com>
2024-06-23 21:35:23 +0700

    resize height screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,12 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
