8a608db7c0781d7b6bea4e5e58e20a973f1defb8
fiebryhoga <dimasfiebry@gmail.com.com>
2024-06-23 21:31:26 +0700

    resize height screen

--- app/pages/khodam/page.tsx
+++ app/pages/khodam/page.tsx
@@ -3,11 +3,11 @@
 
 import React, { useState } from "react";
 import BoxContainer from "@/app/components/BoxContainer";
-// import Footer from "@/app/components/layouts/Footer";
+import Footer from "@/app/components/layouts/Footer";
 
 const CekKhodam = () => {
   return (
-    <div className="flex h-full w-full flex-row bg-green-600 bg-opacity-10">
+    <div className="relative h-full w-full flex flex-col bg-black bg-opacity-10">
       <img
         className="w-full h-full absolute object-cover opacity-35 z-0"
         src="/assets/images/bg.khodam.jpg"
@@ -17,7 +17,7 @@ const CekKhodam = () => {
         <BoxContainer />
       </div>
 
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
