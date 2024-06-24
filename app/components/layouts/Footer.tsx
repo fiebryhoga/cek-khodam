f7ca61208a7f748497dd770486afae5948b3908e
fiebryhoga <dimasfiebry@gmail.com.com>
2024-06-25 00:12:55 +0700

    change link footer

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import { HiChatBubbleLeftEllipsis } from "react-icons/hi2";
 
 const Footer = () => {
   return (
     <footer className=" absolute bottom-0 py-2 w-full bg-white bg-opacity-10 flex flex-col gap-1 justify-center items-center z-50">
       <h1 className="text-xs font-semibold text-white tracking-wide">
-        
         Cek Khodam by DimasKanjeng
       </h1>
-      <Link className="text-xs font-semibold text-green-500 hover:text-green-600 tracking-wide" href="https://sppay.shopee.co.id/qr/00d6c808e377706ecabc">
-        Buat beliin mbah kopi
+      <Link
+        className="flex flex-row gap-1 justify-center items-center text-xs font-semibold text-green-500 hover:text-white tracking-wide"
+        href="https://fiebryhoga.vercel.app"
+      >
+        Konsultasi ke mbah <HiChatBubbleLeftEllipsis color="white" size="15" />
       </Link>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
